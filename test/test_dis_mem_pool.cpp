3308b88e35eac619f2273561e6359f5ccbe93d22
Zhenyuan Ruan <zainryan95@gmail.com>
2021-06-23 18:17:14 -0400

    Update test_dis_mem_pool.cpp.

--- test/test_dis_mem_pool.cpp
+++ test/test_dis_mem_pool.cpp
@@ -18,7 +18,10 @@ using namespace nu;
 
 Runtime::Mode mode;
 
-bool run_test() {
+constexpr static uint32_t kNumThreads = 100;
+constexpr static uint32_t kNumAllocationsPerThread = 1000000;
+
+bool run_single_thread() {
   std::vector<int> a{1, 2, 3, 4, 5, 6};
 
   auto rem_obj = RemObj<ErasedType>::create();
@@ -46,8 +49,54 @@ bool run_test() {
     return false;
   }
 
+  std::vector<RemUniquePtr<int>> unique_ptrs;
+  for (uint64_t i = 0; i < DistributedMemPool::kShardSize / sizeof(int); i++) {
+    unique_ptrs.emplace_back(std::move(dis_mem_pool.allocate_unique<int>()));
+  }
+
   return true;
 }
+
+bool run_multi_thread() {
+  DistributedMemPool dis_mem_pool;
+  std::vector<rt::Thread> alloc_threads;
+  std::vector<rt::Thread> check_threads;
+  std::vector<RemUniquePtr<std::pair<uint64_t, uint64_t>>>
+      unique_ptrs[kNumThreads];
+
+  for (uint32_t i = 0; i < kNumThreads; i++) {
+    alloc_threads.emplace_back([tid = i, &unique_ptrs, &dis_mem_pool] {
+      for (uint32_t j = 0; j < kNumAllocationsPerThread; j++) {
+        unique_ptrs[tid].emplace_back(std::move(
+            dis_mem_pool.allocate_unique<std::pair<uint64_t, uint64_t>>(tid,
+                                                                        j)));
+      }
+    });
+  }
+  for (auto &alloc_thread : alloc_threads) {
+    alloc_thread.Join();
+  }
+
+  bool match = true;
+  for (uint32_t i = 0; i < kNumThreads; i++) {
+    check_threads.emplace_back([tid = i, &match, &unique_ptrs] {
+      for (uint32_t j = 0; j < kNumAllocationsPerThread; j++) {
+        if (*unique_ptrs[tid][j] !=
+            std::make_pair<uint64_t, uint64_t>(tid, j)) {
+          match = false;
+        }
+        unique_ptrs[tid][j].reset();
+      }
+    });
+  }
+  for (auto &check_thread : check_threads) {
+    check_thread.Join();
+  }
+
+  return ACCESS_ONCE(match);
+}
+
+bool run_test() { return run_single_thread() && run_multi_thread(); }
 
 void do_work() {
   if (run_test()) {
