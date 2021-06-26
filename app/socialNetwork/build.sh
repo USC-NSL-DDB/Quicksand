9b7b5ffc2504cef5430102b719334a90234487f3
Zhenyuan Ruan <zainryan95@gmail.com>
2021-06-26 14:33:14 -0600

    Update build.sh to include docker installation.

--- app/socialNetwork/build.sh
+++ app/socialNetwork/build.sh
@@ -113,4 +113,20 @@ cd ../..
 cmake ..
 make -j
 
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent -y
+sudo apt install software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+     -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+exec sudo su -l $USER
+
 popd
