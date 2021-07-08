b70fa67bef8aa4dbae8b0d54ca85f6e831228f9e
Zhenyuan Ruan <zainryan95@gmail.com>
2021-07-08 14:55:06 -0600

    socialNetwork: remove debugging loggings.

--- app/socialNetwork/src/SocialGraphService.h
+++ app/socialNetwork/src/SocialGraphService.h
@@ -44,7 +44,6 @@ SocialGraphService::SocialGraphService(nu::RemObj<UserService>::Cap cap)
 
 void SocialGraphService::Follow(int64_t req_id, int64_t user_id,
                                 int64_t followee_id) {
-  std::cout << "Follow " << user_id << " " << followee_id << std::endl;
   _userid_to_followees_map[user_id].emplace(followee_id);
   _userid_to_followers_map[followee_id].emplace(user_id);
 }
@@ -57,37 +56,25 @@ void SocialGraphService::Unfollow(int64_t req_id, int64_t user_id,
 
 std::vector<int64_t> SocialGraphService::GetFollowers(int64_t req_id,
                                                       int64_t user_id) {
-  std::cout << "GetFollowers " << user_id << std::endl;
   std::vector<int64_t> followers_vec;
   auto &followers_set = _userid_to_followers_map[user_id];
   std::copy(followers_set.begin(), followers_set.end(),
             std::back_inserter(followers_vec));
-  for (auto id : followers_vec) {
-    std::cout << id << ", ";
-  }
-  std::cout << std::endl;
   return followers_vec;
 }
 
 std::vector<int64_t> SocialGraphService::GetFollowees(int64_t req_id,
                                                       int64_t user_id) {
-  std::cout << "GetFollowees " << user_id << std::endl;
   std::vector<int64_t> followees_vec;
   auto &followees_set = _userid_to_followees_map[user_id];
   std::copy(followees_set.begin(), followees_set.end(),
             std::back_inserter(followees_vec));
-  for (auto id : followees_vec) {
-    std::cout << id << ", ";
-  }
-  std::cout << std::endl;
   return followees_vec;
 }
 
 void SocialGraphService::FollowWithUsername(int64_t req_id,
                                             std::string &&user_name,
                                             std::string &&followee_name) {
-  std::cout << "FollowWithUsername " << user_name << " " << followee_name
-            << std::endl;
   auto user_id_future = _user_service_obj.run_async(
       &UserService::GetUserId, req_id, std::move(user_name));
   auto followee_id_future = _user_service_obj.run_async(
