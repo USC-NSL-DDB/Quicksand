2ad22c83373b27cb98104b040bf303ca8179758e
Zhenyuan Ruan <zainryan95@gmail.com>
2021-07-18 18:44:15 -0600

    socialNetwork: move the thrift server into RemObj<ServiceEntry>.

--- app/socialNetwork/src/main.cpp
+++ app/socialNetwork/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <nu/rem_obj.hpp>
 #include <nu/runtime.hpp>
 
 #include "ThriftBackEndServer.hpp"
@@ -6,26 +7,33 @@
 
 using namespace social_network;
 
+class ServiceEntry {
+public:
+  ServiceEntry() {
+    json config_json;
+
+    BUG_ON(LoadConfigFile("config/service-config.json", &config_json) != 0);
+
+    auto port = config_json["back-end-service"]["port"];
+    std::cout << "port = " << port << std::endl;
+    std::shared_ptr<TServerSocket> server_socket =
+        std::make_shared<TServerSocket>("0.0.0.0", port);
+
+    auto secret = config_json["secret"];
+    auto back_end_handler = std::make_shared<ThriftBackEndServer>(secret);
+
+    TThreadedServer server(
+        std::make_shared<BackEndServiceProcessor>(std::move(back_end_handler)),
+        server_socket, std::make_shared<TFramedTransportFactory>(),
+        std::make_shared<TBinaryProtocolFactory>());
+    std::cout << "Starting the ThriftBackEndServer..." << std::endl;
+    server.serve();
+  }
+};
+
 void DoWork() {
-  json config_json;
-
-  BUG_ON(LoadConfigFile("config/service-config.json", &config_json) != 0);
-
-  auto port = config_json["back-end-service"]["port"];
-  std::cout << "port = " << port << std::endl;
-  std::shared_ptr<TServerSocket> server_socket =
-      std::make_shared<TServerSocket>("0.0.0.0", port);
-
-  auto secret = config_json["secret"];
-  auto back_end_handler =
-      std::make_shared<ThriftBackEndServer>(secret);
-
-  TThreadedServer server(
-      std::make_shared<BackEndServiceProcessor>(std::move(back_end_handler)),
-      server_socket, std::make_shared<TFramedTransportFactory>(),
-      std::make_shared<TBinaryProtocolFactory>());
-  std::cout << "Starting the ThriftBackEndServer..." << std::endl;
-  server.serve();
+  auto thrift_back_end_server = nu::RemObj<ServiceEntry>::create_pinned();
+  rt::Yield();
 }
 
 int main(int argc, char **argv) {
