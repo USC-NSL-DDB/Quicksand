82c39768b94eb1b765eda526b3dcbfadde230ef4
Zhenyuan Ruan <zainryan95@gmail.com>
2021-08-11 19:45:52 -0600

    Tuplex: import tuple, start working on it now!

---
+++ app/tuplex/tuplex/python/src/PythonException.cc
@@ -0,0 +1,28 @@
+//--------------------------------------------------------------------------------------------------------------------//
+//                                                                                                                    //
+//                                      Tuplex: Blazing Fast Python Data Science                                      //
+//                                                                                                                    //
+//                                                                                                                    //
+//  (c) 2017 - 2021, Tuplex team                                                                                      //
+//  Created by Leonhard Spiegelberg first on 1/1/2021                                                                 //
+//  License: Apache 2.0                                                                                               //
+//--------------------------------------------------------------------------------------------------------------------//
+
+#include <PythonException.h>
+#include <iostream>
+
+namespace tuplex {
+
+    void translateCCException(const PythonException& e) {
+        // this is buggy... --> switch to pybind11?
+        std::cerr<<"C++ exception raised: "<<e.what()<<std::endl;
+	    std::cerr.flush();
+
+#ifndef NDEBUG
+        printf("C++ exception raised: %s", e.what());
+#endif
+
+
+        PyErr_SetString(PyExc_Exception, e.what());
+    }
+}
