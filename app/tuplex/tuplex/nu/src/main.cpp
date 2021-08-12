42295804372b1aa1f877c52b21c85d029128da12
Zhenyuan Ruan <zainryan95@gmail.com>
2021-08-12 10:28:03 -0600

    tuplex: successfully link nu, caladan, and tuplex statically together.

--- app/tuplex/tuplex/nu/src/main.cpp
+++ app/tuplex/tuplex/nu/src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <runtime.h>
 
-int main() {
-  std::cout << "Hello world!" << std::endl;
+int main(int argc, char **argv) {
+  int ret;
+
+  if (argc < 2) {
+    std::cerr << "usage: [cfg_file]" << std::endl;
+    return -EINVAL;
+  }
+
+  ret = rt::RuntimeInit(std::string(argv[1]),
+                        [] { std::cout << "Hello world!" << std::endl; });
+
+  if (ret) {
+    std::cerr << "failed to start runtime" << std::endl;
+    return ret;
+  }
+
   return 0;
 }
