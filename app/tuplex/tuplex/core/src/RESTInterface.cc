f5f1f1fdf6a8a4bdb19ebdfab631b451102c17e1
Zhenyuan Ruan <zainryan95@gmail.com>
2021-08-12 15:59:15 -0600

    tuplex: no longer depends on libcurl since it's hard to be fully static.

--- app/tuplex/tuplex/core/src/RESTInterface.cc
+++ app/tuplex/tuplex/core/src/RESTInterface.cc
@@ -16,41 +16,42 @@
 static bool curlInitialized = false;
 
 void RESTInterface::init() {
-    if(!curlInitialized)
-        // change to include ssl
-        curl_global_init(CURL_GLOBAL_NOTHING);
+    // if(!curlInitialized)
+    //     // change to include ssl
+    //     curl_global_init(CURL_GLOBAL_NOTHING);
     curlInitialized = true;
 }
 
 void RESTInterface::shutdown() {
-    if(curlInitialized)
-        curl_global_cleanup();
+    // if(curlInitialized)
+    //     curl_global_cleanup();
     curlInitialized = false;
 }
 
 
 CURL* RESTInterface::getCurlHandle() {
-    CURL *handle = curl_easy_init();
+//     CURL *handle = curl_easy_init();
 
-    // send data to this function
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writeCallback);
+//     // send data to this function
+//     curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writeCallback);
 
-    // pass argument to callback
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void*)&_chunk);
+//     // pass argument to callback
+//     curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void*)&_chunk);
 
-    // to avoid requests being blocked, set default user agent
-    curl_easy_setopt(handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+//     // to avoid requests being blocked, set default user agent
+//     curl_easy_setopt(handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
 
-    // // set timeout to 500ms
-    // auto timeout = 500L;
-    // curl_easy_setopt(handle, CURLOPT_TIMEOUT_MS, timeout);
-    // curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT_MS, timeout);
-    // curl_easy_setopt(handle, CURLOPT_ACCEPTTIMEOUT_MS, timeout);
+//     // // set timeout to 500ms
+//     // auto timeout = 500L;
+//     // curl_easy_setopt(handle, CURLOPT_TIMEOUT_MS, timeout);
+//     // curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT_MS, timeout);
+//     // curl_easy_setopt(handle, CURLOPT_ACCEPTTIMEOUT_MS, timeout);
 
-#ifndef NDEBUG
-    // curl_easy_setopt(_handle, CURLOPT_VERBOSE, 1L);
-#endif
-    return handle;
+// #ifndef NDEBUG
+//     // curl_easy_setopt(_handle, CURLOPT_VERBOSE, 1L);
+// #endif
+//     return handle;
+    return nullptr;
 }
 
 RESTInterface::RESTInterface()  {
@@ -64,7 +65,7 @@ RESTInterface::RESTInterface()  {
 
     // init json list
     _jsonSList = nullptr;
-    _jsonSList = curl_slist_append(_jsonSList, "Content-Type: application/json");
+    // _jsonSList = curl_slist_append(_jsonSList, "Content-Type: application/json");
 
     _statusCode = 0;
 }
@@ -89,7 +90,7 @@ size_t RESTInterface::writeCallback(void *contents, size_t size, size_t nmemb, v
 
 RESTInterface::~RESTInterface() {
     assert(_jsonSList);
-    curl_slist_free_all(_jsonSList);
+    // curl_slist_free_all(_jsonSList);
 
     if(_chunk.memory)
        free(_chunk.memory);
@@ -100,51 +101,53 @@ RESTInterface::~RESTInterface() {
 std::string RESTInterface::get(const std::string &url,
                                std::function<void(const std::string &)> errCallback) {
 
-    auto handle = getCurlHandle();
+    // auto handle = getCurlHandle();
 
-    curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+    // curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
 
-    auto res = curl_easy_perform(handle);
+    // auto res = curl_easy_perform(handle);
 
-    std::string str;
-    if(res != CURLE_OK) {
-        errCallback(std::string(curl_easy_strerror(res)));
-        return "";
-    } else {
-        // convert to string and return
-        str.reserve(_chunk.size);
-        str.assign((char*)_chunk.memory, _chunk.size);
-    }
+    // std::string str;
+    // if(res != CURLE_OK) {
+    //     errCallback(std::string(curl_easy_strerror(res)));
+    //     return "";
+    // } else {
+    //     // convert to string and return
+    //     str.reserve(_chunk.size);
+    //     str.assign((char*)_chunk.memory, _chunk.size);
+    // }
 
-    curl_easy_cleanup(handle);
-    return str;
+    // curl_easy_cleanup(handle);
+    // return str;
+    return "";
 }
 
 std::string RESTInterface::postJSON(const std::string &url, const std::string &content,
                                     std::function<void(const std::string &)> errCallback) {
-    auto handle = getCurlHandle();
+    // auto handle = getCurlHandle();
 
-    curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
-    /* size of the POST data */
-    curl_easy_setopt(handle, CURLOPT_POSTFIELDSIZE, content.length());
+    // curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+    // /* size of the POST data */
+    // curl_easy_setopt(handle, CURLOPT_POSTFIELDSIZE, content.length());
 
-    /* pass in a pointer to the data - libcurl will not copy */
-    curl_easy_setopt(handle, CURLOPT_POSTFIELDS, content.c_str());
+    // /* pass in a pointer to the data - libcurl will not copy */
+    // curl_easy_setopt(handle, CURLOPT_POSTFIELDS, content.c_str());
 
-    curl_easy_setopt(handle, CURLOPT_HTTPHEADER, _jsonSList);
+    // curl_easy_setopt(handle, CURLOPT_HTTPHEADER, _jsonSList);
 
-    auto res = curl_easy_perform(handle);
+    // auto res = curl_easy_perform(handle);
 
-    std::string str;
-    if (res != CURLE_OK) {
-        errCallback(curl_easy_strerror(res));
-        return "";
-    } else {
-        // convert to string and return
-        str.reserve(_chunk.size);
-        str.assign((char *) _chunk.memory, _chunk.size);
-    }
+    // std::string str;
+    // if (res != CURLE_OK) {
+    //     errCallback(curl_easy_strerror(res));
+    //     return "";
+    // } else {
+    //     // convert to string and return
+    //     str.reserve(_chunk.size);
+    //     str.assign((char *) _chunk.memory, _chunk.size);
+    // }
 
-    curl_easy_cleanup(handle);
-    return str;
+    // curl_easy_cleanup(handle);
+    // return str;
+    return "";
 }
