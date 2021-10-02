764bf99a3f36a16c12dcd43349e194463f9a3ffa
Zhenyuan Ruan <zainryan95@gmail.com>
2021-10-02 10:00:27 -0600

    exp: add scripts for nu-version multi-node socialnetwork exp.

---
+++ exp/social_net/nu-rewritten-multi/run.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+source ../../shared.sh
+
+DIR=`pwd`
+mkdir logs
+rm -rf logs/*
+
+set_bridge $CONTROLLER_ETHER
+set_bridge $CLIENT1_ETHER
+
+NGINX_SERVER_IP=$SERVER7_IP
+NGINX_SERVER_CALADAN_IP_AND_MASK=18.18.1.254/24
+NGINX_SERVER_NIC=ens1f0
+SOCIAL_NET_DIR=`pwd`/../../../app/socialNetwork/single_obj/
+
+cd $SOCIAL_NET_DIR
+./build.sh
+
+ssh $NGINX_SERVER_IP "sudo apt-get install -y python3-pip; pip3 install aiohttp"
+
+ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./install_docker.sh"
+ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./down_nginx.sh; ./up_nginx.sh"
+ssh $NGINX_SERVER_IP "sudo ip addr add $NGINX_SERVER_CALADAN_IP_AND_MASK dev $NGINX_SERVER_NIC"
+
+mops=( 1 1.4 1.9 2.3 )
+
+for num_worker_nodes in `seq 1 4`
+do    
+    cd $SOCIAL_NET_DIR
+    sed "s/constexpr uint32_t kNumEntryObjs.*/constexpr uint32_t kNumEntryObjs = $num_worker_nodes;/g" \
+	-i src/main.cpp
+    sed "s/constexpr static uint32_t kNumEntryObjs.*/constexpr static uint32_t kNumEntryObjs = $num_worker_nodes;/g" \
+	-i bench/client.cpp
+    mop=${mops[`expr $num_worker_nodes - 1`]}
+    sed "s/constexpr static double kTargetMops.*/constexpr static double kTargetMops = $mop;/g" \
+	-i bench/client.cpp
+    cd build
+    make clean
+    make -j
+    for i in `seq 1 $num_worker_nodes`
+    do
+        ip=${REMOTE_SERVER_IPS[`expr $i - 1`]}
+	ssh $ip "mkdir -p `pwd`/src"
+	scp src/main $ip:`pwd`/src
+    done
+    sudo $NU_DIR/caladan/iokerneld &
+    sleep 5
+    cd ..
+    sudo build/src/main $DIR/conf/controller CTL 18.18.1.3 &
+    sleep 5
+    for i in `seq 1 $num_worker_nodes`
+    do
+        ip=${REMOTE_SERVER_IPS[`expr $i - 1`]}
+	ssh $ip "sudo $NU_DIR/caladan/iokerneld" &
+	conf=$DIR/conf/server$i
+	sleep 5
+	ssh $ip "cd $SOCIAL_NET_DIR; sudo build/src/main $conf SRV 18.18.1.3" &
+    done    
+    sleep 5
+    sudo build/src/main $DIR/conf/client1 CLT 18.18.1.3 &
+    sleep 5
+    ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; python3 scripts/init_social_graph.py"
+    sleep 5
+    sudo build/bench/client $DIR/conf/client2 CLT 18.18.1.3 1>$DIR/logs/$num_worker_nodes 2>&1
+    for i in `seq 1 $num_worker_nodes`
+    do
+        ip=${REMOTE_SERVER_IPS[`expr $i - 1`]}
+	ssh $ip "sudo pkill -9 iokerneld"
+	ssh $ip "sudo pkill -9 main"
+    done
+    sudo pkill -9 iokerneld    
+    sudo pkill -9 main
+done
+
+unset_bridge $CONTROLLER_ETHER
+unset_bridge $CLIENT1_ETHER
+
+ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./down_nginx.sh;"
+ssh $NGINX_SERVER_IP "sudo ip addr delete $NGINX_SERVER_CALADAN_IP_AND_MASK dev $NGINX_SERVER_NIC"
+ssh $NGINX_SERVER_IP "docker rm -vf $(docker ps -aq)"
+ssh $NGINX_SERVER_IP "docker rmi -f $(docker images -aq)"
+ssh $NGINX_SERVER_IP "docker volume prune -f"
