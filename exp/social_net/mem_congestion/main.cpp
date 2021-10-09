6619a615ead81a3f5ab352da821b2d31d1fe3ebb
Zhenyuan Ruan <zainryan95@gmail.com>
2021-10-09 14:46:04 -0600

    exp: add code and scripts of the socialnet mem congestion exp.

---
+++ exp/social_net/mem_congestion/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <memory>
+#include <nu/monitor.hpp>
+#include <nu/rem_obj.hpp>
+#include <nu/runtime.hpp>
+
+#include "ThriftBackEndServer.hpp"
+
+using namespace social_network;
+
+const static uint32_t kEntryObjIps[] = {MAKE_IP_ADDR(18, 18, 1, 2),
+                                        MAKE_IP_ADDR(18, 18, 1, 5)};
+
+bool signalled = false;
+
+class ServiceEntry {
+public:
+  ServiceEntry(StateCaps caps) {
+    json config_json;
+
+    BUG_ON(LoadConfigFile("config/service-config.json", &config_json) != 0);
+
+    auto port = config_json["back-end-service"]["port"];
+    std::cout << "port = " << port << std::endl;
+    std::shared_ptr<TServerSocket> server_socket =
+        std::make_shared<TServerSocket>("0.0.0.0", port);
+
+    caps.secret = config_json["secret"];
+    auto back_end_handler = std::make_shared<ThriftBackEndServer>(caps);
+
+    server_ = std::make_unique<TThreadedServer>(
+        std::make_shared<BackEndServiceProcessor>(std::move(back_end_handler)),
+        server_socket, std::make_shared<TFramedTransportFactory>(),
+        std::make_shared<TBinaryProtocolFactory>());
+  }
+
+  void reserve_conn(uint32_t ip) { nu::Runtime::reserve_conn(ip); }
+
+  void start() {
+    std::cout << "Starting the ThriftBackEndServer..." << std::endl;
+    server_->serve();
+  }
+
+private:
+  std::unique_ptr<TThreadedServer> server_;
+};
+
+void wait_for_signal() {
+  rt::access_once(signalled) = false;
+  while (!rt::access_once(signalled)) {
+    timer_sleep(100);
+  }
+}
+
+void do_work() {
+  auto states = std::make_unique<States>();
+
+  std::cout << "creating proxy 0..." << std::endl;
+  netaddr raddr = {.ip = kEntryObjIps[0],
+                   .port = nu::ObjServer::kObjServerPort};
+  auto service_entry_0 =
+      nu::RemObj<ServiceEntry>::create_pinned_at(raddr, states->get_caps());
+
+  std::cout << "waiting for signal..." << std::endl;
+  wait_for_signal();
+
+  std::cout << "creating proxy 1..." << std::endl;
+  raddr.ip = kEntryObjIps[1];
+  auto service_entry_1 =
+      nu::RemObj<ServiceEntry>::create_pinned_at(raddr, states->get_caps());
+
+  service_entry_0.run(&ServiceEntry::reserve_conn, kEntryObjIps[0]);
+  service_entry_0.run(&ServiceEntry::reserve_conn, kEntryObjIps[1]);
+  service_entry_1.run(&ServiceEntry::reserve_conn, kEntryObjIps[1]);  
+
+  auto future_0 = service_entry_0.run_async(&ServiceEntry::start);
+  auto future_1 = service_entry_1.run_async(&ServiceEntry::start);
+}
+
+void signal_handler(int signum) { rt::access_once(signalled) = true; }
+
+int main(int argc, char **argv) {
+  signal(SIGHUP, signal_handler);
+  return nu::runtime_main_init(argc, argv, [](int, char **) { do_work(); });
+}
---
+++ exp/social_net/mem_congestion/run.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+source ../../shared.sh
+
+mkdir logs
+rm -rf logs/*
+
+set_bridge $CONTROLLER_ETHER
+set_bridge $CLIENT1_ETHER
+
+SRC_SERVER_IP=$SERVER3_IP
+DEST_SERVER_IP=$SERVER4_IP
+CLIENT_IP=$SERVER5_IP
+NGINX_IP=$SERVER7_IP
+NGINX_SERVER_CALADAN_IP_AND_MASK=18.18.1.254/24
+NGINX_SERVER_NIC=ens1f0
+SOCIAL_NET_DIR=`pwd`/../../../app/socialNetwork/single_obj/
+
+ssh $NGINX_IP "sudo apt-get update; sudo apt-get install -y python3-pip; pip3 install aiohttp"
+
+ssh $NGINX_IP "cd $SOCIAL_NET_DIR; ./install_docker.sh"
+ssh $NGINX_IP "cd $SOCIAL_NET_DIR; ./down_nginx.sh; ./up_nginx.sh"
+ssh $NGINX_IP "sudo ip addr add $NGINX_SERVER_CALADAN_IP_AND_MASK dev $NGINX_SERVER_NIC"
+
+DIR=`pwd`
+cd $SOCIAL_NET_DIR
+mv src/main.cpp src/main.cpp.bak
+mv bench/client.cpp bench/client.cpp.bak
+cp $DIR/main.cpp src/main.cpp
+cp $DIR/client.cpp bench/client.cpp
+cd build
+make clean
+make -j
+cd ..
+scp build/src/main $SRC_SERVER_IP:`pwd`/build/src
+scp build/src/main $DEST_SERVER_IP:`pwd`/build/src
+scp build/bench/client $CLIENT_IP:`pwd`/build/bench
+
+sudo $NU_DIR/caladan/iokerneld &
+sleep 5
+sudo build/src/main $DIR/conf/controller CTL 18.18.1.3 &
+ssh $SRC_SERVER_IP "cd $DIR; source ../../shared.sh; set_bridge $SERVER1_ETHER"
+ssh $SRC_SERVER_IP "sudo $NU_DIR/caladan/iokerneld" &
+sleep 5
+ssh $SRC_SERVER_IP "cd `pwd`; sudo build/src/main $DIR/conf/server1 SRV 18.18.1.3" &
+sleep 5
+sudo build/src/main $DIR/conf/client1 CLT 18.18.1.3 &
+ssh $DEST_SERVER_IP "cd $DIR; source ../../shared.sh; set_bridge $SERVER2_ETHER"
+ssh $DEST_SERVER_IP "sudo $NU_DIR/caladan/iokerneld" &
+sleep 5
+ssh $DEST_SERVER_IP "cd `pwd`; sudo build/src/main $DIR/conf/server2 SRV 18.18.1.3" &
+sleep 5
+sudo pkill -SIGHUP main
+sleep 5
+ssh $NGINX_IP "cd `pwd`; python3 scripts/init_social_graph.py"
+ssh $SRC_SERVER_IP "cd $DIR; sudo stdbuf -o0 ../../../bin/bench_real_mem_pressure conf/client3" >$DIR/logs/.pressure &
+pid_pressure=$!
+( tail -f -n0 $DIR/logs/.pressure & ) | grep -q "waiting for signal"
+ssh $CLIENT_IP "sudo $NU_DIR/caladan/iokerneld" &
+sleep 5
+ssh $CLIENT_IP "cd `pwd`; sudo build/bench/client $DIR/conf/client2" &
+pid_client=$!
+sleep 15
+ssh $SRC_SERVER_IP "sudo pkill -SIGHUP bench_real_mem"
+wait $pid_client
+ssh $SRC_SERVER_IP "sudo pkill -SIGHUP bench_real_mem"
+wait $pid_pressure
+scp $CLIENT_IP:$SOCIAL_NET_DIR/timeseries $DIR/logs/
+scp $SRC_SERVER_IP:$DIR/alloc_mem_traces $DIR/logs/
+scp $SRC_SERVER_IP:$DIR/avail_mem_traces $DIR/logs/
+
+mv src/main.cpp.bak src/main.cpp
+mv bench/client.cpp.bak bench/client.cpp
+
+sudo pkill -9 iokerneld; sudo pkill -9 main
+ssh $CLIENT_IP "sudo pkill -9 iokerneld;"
+ssh $SRC_SERVER_IP "sudo pkill -9 iokerneld; sudo pkill -9 main; sudo pkill -9 bench"
+ssh $DEST_SERVER_IP "sudo pkill -9 iokerneld; sudo pkill -9 main"
+ssh $NGINX_IP "cd `pwd`; ./down_nginx.sh;"
+ssh $NGINX_IP "sudo ip addr delete $NGINX_SERVER_CALADAN_IP_AND_MASK dev $NGINX_SERVER_NIC"
+ssh $NGINX_IP "docker rm -vf $(docker ps -aq)"
+ssh $NGINX_IP "docker rmi -f $(docker images -aq)"
+ssh $NGINX_IP "docker volume prune -f"
+
+unset_bridge $CONTROLLER_ETHER
+unset_bridge $CLIENT1_ETHER
+ssh $SRC_SERVER_IP "cd $DIR; source ../../shared.sh; unset_bridge $SERVER1_ETHER"
+ssh $DEST_SERVER_IP "cd $DIR; source ../../shared.sh; unset_bridge $SERVER2_ETHER"
