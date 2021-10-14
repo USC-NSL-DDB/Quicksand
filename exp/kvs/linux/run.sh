af9eaf0c604e0fd184f2cbfb9874dbd4b3a66f82
Zhenyuan Ruan <zainryan95@gmail.com>
2021-10-14 14:53:15 -0600

    exp: fix the script of linux kvs exp.

--- exp/kvs/linux/run.sh
+++ exp/kvs/linux/run.sh
@@ -3,7 +3,7 @@
 source ../../shared.sh
 
 MEMCACHED_IP=$SERVER2_IP
-MEMCACHED_SIB_IP=$(to_sib_addr $MEMCACHED_IP)
+MEMCACHED_SIB_IP=$(to_100g_addr $MEMCACHED_IP)
 DIR=`pwd`
 rm -rf log
 
@@ -22,6 +22,7 @@ SET_NIC_CMD+="sudo sysctl net.ipv4.tcp_syncookies=1;"
 SET_NIC_CMD+="sudo ifconfig $DPDK_NIC up;"
 
 ssh $MEMCACHED_IP "$SET_NIC_CMD"
+ssh $MEMCACHED_IP "sudo apt-get install -y libevent-dev"
 ssh $MEMCACHED_IP "cd `pwd`; wget http://www.memcached.org/files/memcached-1.6.12.tar.gz"
 ssh $MEMCACHED_IP "cd `pwd`; tar xvf memcached-1.6.12.tar.gz"
 ssh $MEMCACHED_IP "cd `pwd`/memcached-1.6.12; ./configure; make -j"
@@ -32,10 +33,14 @@ ssh $MEMCACHED_IP "cd `pwd`/memcached-1.6.12; \
 sudo $NU_DIR/caladan/iokerneld &
 sleep 5
 cd ../../../caladan/apps/synthetic/
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+rustup default nightly
+sudo apt-get install -y clang
 cargo clean
 cargo build --release
 sudo target/release/synthetic $MEMCACHED_SIB_IP:8888 --config $DIR/conf/client --mode runtime-client \
-     --protocol memcached --transport tcp --mpps 4 --samples 20 --threads 48 --start_mpps 0.2 \
-     --runtime=10 --nvalues=107374182 --key_size 20 --value_size 2 >$DIR/log
+     --protocol memcached --transport tcp --mpps 6 --samples 20 --threads 48 --start_mpps 0.2 \
+     --runtime=5 --nvalues=107374182 --key_size 20 --value_size 2 >$DIR/log
 sudo pkill -9 iokerneld
 ssh $MEMCACHED_IP "sudo pkill -9 memcached"
