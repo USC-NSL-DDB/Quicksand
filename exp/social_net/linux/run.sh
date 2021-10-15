68348beefb0aa67e6e4538b9dd3eca095b1a40bb
Zhenyuan Ruan <zainryan95@gmail.com>
2021-10-14 18:15:19 -0600

    exp: fix the script of linux-version single-node socialnet exp.

--- exp/social_net/linux/run.sh
+++ exp/social_net/linux/run.sh
@@ -9,7 +9,7 @@ rm -rf logs/*
 NGINX_SERVER_IP=$SERVER7_IP
 SOCIAL_NET_DIR=`pwd`/../../../app/socialNetwork/orig/
 
-NGINX_SERVER_SIB_IP=$(to_sib_addr $NGINX_SERVER_IP)
+NGINX_SERVER_SIB_IP=$(to_100g_addr $NGINX_SERVER_IP)
 CALADAN_IP=`echo $NGINX_SERVER_SIB_IP | sed "s/\(.*\)\.\(.*\)\.\(.*\)\.\(.*\)/\1\.\2\.\3\.254/g"`
 sed "s/host_addr.*/host_addr $CALADAN_IP/g" -i conf/client.conf
 
@@ -19,6 +19,20 @@ sed "s/constexpr static char kHostIP.*/constexpr static char kHostIP[] = \"$NGIN
 ./build.sh
 
 ssh $NGINX_SERVER_IP "sudo apt-get update; sudo apt-get install -y python3-pip; pip3 install aiohttp"
+ssh $NGINX_SERVER_IP "sudo service irqbalance stop"
+ssh $NGINX_SERVER_IP "cd `pwd`/../../../caladan/scripts/; sudo ./set_irq_affinity 0-47 mlx5"
+SET_NIC_CMD="sudo ifconfig $DPDK_NIC down;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC adaptive-rx off;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC adaptive-tx off;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC rx-usecs 0;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC rx-frames 0;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC tx-usecs 0;"
+SET_NIC_CMD+="sudo ethtool -C $DPDK_NIC tx-frames 0;"
+SET_NIC_CMD+="sudo ethtool -N $DPDK_NIC rx-flow-hash udp4 sdfn;"
+SET_NIC_CMD+="sudo sysctl net.ipv4.tcp_syncookies=1;"
+SET_NIC_CMD+="sudo ifconfig $DPDK_NIC up;"
+ssh $NGINX_SERVER_IP "$SET_NIC_CMD"
+ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./install_docker.sh"
 
 mops=( 0.002 0.003 0.005 0.007 0.009 )
 
