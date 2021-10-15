5ce61c7b796ef31c906650b5bbfd80b6d4d4649a
Zhenyuan Ruan <zainryan95@gmail.com>
2021-10-15 09:45:30 -0600

    exp: update shared.sh.

--- exp/shared.sh
+++ exp/shared.sh
@@ -5,9 +5,9 @@ CONTROLLER_ETHER=1E:CF:16:43:AF:95
 CLIENT1_ETHER=1E:CF:16:43:AF:94
 SERVER1_ETHER=1E:CF:16:43:AF:96
 SERVER2_ETHER=1E:CF:16:43:AF:97
-SERVER1_IP=10.10.1.3
-SERVER2_IP=10.10.1.1
-SERVER3_IP=10.10.1.2
+SERVER1_IP=10.10.1.1
+SERVER2_IP=10.10.1.2
+SERVER3_IP=10.10.1.3
 SERVER4_IP=10.10.1.4
 SERVER5_IP=10.10.1.5
 SERVER6_IP=10.10.1.6
@@ -26,6 +26,6 @@ function unset_bridge {
     sudo bridge fdb delete $1 self dev $DPDK_NIC 2>/dev/null
 }
 
-function to_sib_addr {
+function to_100g_addr {
     echo `echo $1 | sed "s/10\.10\.1\(.*\)/10\.10\.2\1/g"`
 }
