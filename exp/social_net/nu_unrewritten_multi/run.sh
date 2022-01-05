b4882b8a23ecca7751b9bd2796548436860982ab
Zhenyuan Ruan <zainryan95@gmail.com>
2022-01-05 10:25:38 -0700

    exp: update the scripts of unrewrriten-socialnet one to run with up to 15 machines.

--- exp/social_net/nu_unrewritten_multi/run.sh
+++ exp/social_net/nu_unrewritten_multi/run.sh
@@ -3,13 +3,16 @@
 source ../../shared.sh
 
 DIR=`pwd`
+CTRL_IP=18.18.1.3
+LPID=1
+
 mkdir logs
 rm -rf logs/*
 
 set_bridge $CONTROLLER_ETHER
 set_bridge $CLIENT1_ETHER
 
-NGINX_SERVER_IP=$SERVER7_IP
+NGINX_SERVER_IP=$SERVER15_IP
 NGINX_SERVER_CALADAN_IP_AND_MASK=18.18.1.254/24
 NGINX_SERVER_NIC=ens1f0
 SOCIAL_NET_DIR=`pwd`/../../../app/socialNetwork/multi_objs/
@@ -23,9 +26,9 @@ ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./install_docker.sh"
 ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; ./down_nginx.sh; ./up_nginx.sh"
 ssh $NGINX_SERVER_IP "sudo ip addr add $NGINX_SERVER_CALADAN_IP_AND_MASK dev $NGINX_SERVER_NIC"
 
-mops=( 1 1.2 1.4 1.6 )
+mops=( 1 1.2 1.4 1.6 1.8 2 2.2 2.4 2.6 2.8 3 3.2 3.4)
 
-for num_worker_nodes in `seq 1 4`
+for num_worker_nodes in `seq 1 13`
 do
     cd $SOCIAL_NET_DIR
     sed "s/constexpr static uint32_t kNumEntryObjs.*/constexpr static uint32_t kNumEntryObjs = $num_worker_nodes;/g" \
@@ -47,7 +50,7 @@ do
     sudo $NU_DIR/caladan/iokerneld &
     sleep 5
     cd ..
-    sudo build/src/BackEndService $DIR/conf/controller CTL 18.18.1.3 &
+    sudo $NU_DIR/bin/ctrl_main $DIR/conf/controller $CTRL_IP &
     sleep 5
     for i in `seq 1 $num_worker_nodes`
     do
@@ -55,22 +58,23 @@ do
 	ssh $ip "sudo $NU_DIR/caladan/iokerneld" &
 	conf=$DIR/conf/server$i
 	sleep 5
-	ssh $ip "cd $SOCIAL_NET_DIR; sudo build/src/BackEndService $conf SRV 18.18.1.3" &
+	ssh $ip "cd $SOCIAL_NET_DIR; sudo build/src/BackEndService $conf SRV $CTRL_IP $LPID"&
     done
     sleep 5
-    sudo build/src/BackEndService $DIR/conf/client1 CLT 18.18.1.3 &
+    sudo build/src/BackEndService $DIR/conf/client1 CLT $CTRL_IP $LPID &
     sleep 5
     ssh $NGINX_SERVER_IP "cd $SOCIAL_NET_DIR; python3 scripts/init_social_graph.py"
     sleep 5
-    sudo build/bench/client $DIR/conf/client2 CLT 18.18.1.3 1>$DIR/logs/$num_worker_nodes 2>&1
+    sudo build/bench/client $DIR/conf/client2 CLT 1>$DIR/logs/$num_worker_nodes 2>&1
     for i in `seq 1 $num_worker_nodes`
     do
         ip=${REMOTE_SERVER_IPS[`expr $i - 1`]}
-	ssh $ip "sudo pkill -9 iokerneld"
-	ssh $ip "sudo pkill -9 BackEndService"
+        ssh $ip "sudo pkill -9 iokerneld"
+        ssh $ip "sudo pkill -9 BackEndService"
     done
     sudo pkill -9 iokerneld
     sudo pkill -9 BackEndService
+    sudo pkill -9 ctrl_main
 done
 
 unset_bridge $CONTROLLER_ETHER
