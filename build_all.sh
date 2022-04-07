e5acd549d14d307da1da0eae742426dcc32c69b3
Zhenyuan Ruan <zainryan95@gmail.com>
2022-04-07 11:22:58 -0600

    nu: remove glibc from build.sh.

--- build_all.sh
+++ build_all.sh
@@ -24,20 +24,6 @@ if [ $NODE_TYPE == "xl170-uswitch" ]; then
     patch -p1 -d caladan/ < caladan/build/connectx-4.patch    
 fi
 
-export glibc_install="$(pwd)/glibc/build/install"
-git clone git://sourceware.org/git/glibc.git
-cd glibc
-git checkout glibc-2.32
-mkdir build
-cd build
-LD_LIBRARY_PATH_BAK=$LD_LIBRARY_PATH
-unset LD_LIBRARY_PATH
-../configure --prefix "$glibc_install" --enable-static-nss
-make -j `nproc`
-make install -j `nproc`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_BAK
-cd ../..
-
 cd caladan
 ./build.sh
 cd ..
