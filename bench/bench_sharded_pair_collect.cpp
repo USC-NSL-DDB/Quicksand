02ec2370054b8f236157ec3a96d5fc0e8fb52c10
Zhenyuan Ruan <zainryan95@gmail.com>
2022-06-26 21:58:45 -0600

    bench: update bench_sharded_pair_collect.cpp.

--- bench/bench_sharded_pair_collect.cpp
+++ bench/bench_sharded_pair_collect.cpp
@@ -9,56 +9,70 @@
 
 constexpr uint32_t kRunTimes = 1;
 constexpr uint32_t kNumElements = 160 << 20;
-constexpr uint32_t kNumThreads = nu::kNumCores * 2;
+constexpr uint32_t kNumThreads = 4 * (nu::kNumCores - 2);
 
 class Work {
  public:
   Work() {
     for (uint32_t i = 0; i < kRunTimes; i++) {
       std::cout << "Running No." << i << " time..." << std::endl;
-      single_thread();
+      single_thread_std_vector();
+      single_thread_no_partition();
+      single_thread_perfect_partition();
       multi_threads_no_partition();
       multi_threads_perfect_partition();
       // TODO: add more.
     }
   }
 
-  void single_thread() {
-    std::cout << "\tRunning single-thread bench..." << std::endl;
-    {
-      auto sc = nu::make_sharded_pair_collection<int, int>();
-      auto t0 = microtime();
-      for (uint32_t i = 0; i < kNumElements; i++) {
-        sc.emplace(i, i);
-      }
-      auto t1 = microtime();
-      std::cout << "\t\tShardedPairCollection: "
-                << static_cast<double>(kNumElements) / (t1 - t0) << " MOPS"
-                << std::endl;
-    }
+  void single_thread_std_vector() {
+    std::cout << "\tRunning single-thread-std-vector ench..." << std::endl;
 
-    {
-      nu::RuntimeSlabGuard slab;
-      std::vector<std::pair<int, int>> v;
-      auto t0 = microtime();
-      for (uint32_t i = 0; i < kNumElements; i++) {
-        v.emplace_back(i, i);
-      }
-      auto t1 = microtime();
-      std::cout << "\t\tstd::vector: "
-                << static_cast<double>(kNumElements) / (t1 - t0) << " MOPS"
-                << std::endl;
+    nu::RuntimeSlabGuard slab;
+    std::vector<std::pair<int, int>> v;
+    auto t0 = microtime();
+    for (uint32_t i = 0; i < kNumElements; i++) {
+      v.emplace_back(i, i);
     }
+    auto t1 = microtime();
+    std::cout << "\t\tstd::vector: "
+              << static_cast<double>(kNumElements) / (t1 - t0) << " MOPS"
+              << std::endl;
+  }
+
+  void single_thread_no_partition() {
+    std::cout << "\tRunning single-thread-no-partition bench..." << std::endl;
+    auto sc = nu::make_sharded_pair_collection<int, int>();
+    single_thread(&sc);
+  }
+
+  void single_thread_perfect_partition() {
+    std::cout << "\tRunning single-thread-perfect-partition bench..."
+              << std::endl;
+    auto sc = nu::make_sharded_pair_collection<int, int>(
+        kNumElements, 0, [](int &x, uint64_t offset) { x += offset; });
+    single_thread(&sc);
+  }
+
+  void single_thread(nu::ShardedPairCollection<int, int> *sc) {
+    auto t0 = microtime();
+    for (uint32_t i = 0; i < kNumElements; i++) {
+      sc->emplace(i, i);
+    }
+    auto t1 = microtime();
+    std::cout << "\t\tShardedPairCollection: "
+              << static_cast<double>(kNumElements) / (t1 - t0) << " MOPS"
+              << std::endl;
   }
 
   void multi_threads_no_partition() {
-    std::cout << "\tRunning multi-thread-no-partition bench..." << std::endl;
+    std::cout << "\tRunning multi-threads-no-partition bench..." << std::endl;
     auto sc = nu::make_sharded_pair_collection<int, int>();
     multi_threads(&sc);
   }
 
   void multi_threads_perfect_partition() {
-    std::cout << "\tRunning multi-thread-perfect-partition bench..."
+    std::cout << "\tRunning multi-threads-perfect-partition bench..."
               << std::endl;
     auto sc = nu::make_sharded_pair_collection<int, int>(
         kNumElements, 0, [](int &x, uint64_t offset) { x += offset; });
@@ -71,7 +85,7 @@ class Work {
       ths.emplace_back([sc, tid = i] {
         auto num_elems_per_th = kNumElements / kNumThreads;
         for (uint32_t i = 0; i < num_elems_per_th; i++) {
-          sc->emplace(i * kNumThreads + tid, i);
+          sc->emplace(tid * num_elems_per_th + i, i);
         }
       });
     }
