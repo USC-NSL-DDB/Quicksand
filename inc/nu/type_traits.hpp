a4498b0f3f9f5c97f8d0230a0414081d579f0c57
Zhenyuan Ruan <zainryan95@gmail.com>
2022-07-17 13:42:06 -0400

    nu: add is_base_of_template_v.

--- inc/nu/type_traits.hpp
+++ inc/nu/type_traits.hpp
@@ -17,6 +17,19 @@ constexpr bool is_specialization_of_v =
 template <typename T>
 auto move_if_safe(T &&t);
 
+template <template <typename...> class C, typename... Ts>
+std::true_type is_base_of_template_impl(const C<Ts...> *);
+
+template <template <typename...> class C>
+std::false_type is_base_of_template_impl(...);
+
+template <typename T, template <typename...> class C>
+using is_base_of_template =
+    decltype(is_base_of_template_impl<C>(std::declval<T *>()));
+
+template <typename T, template <typename...> class C>
+inline constexpr bool is_base_of_template_v = is_base_of_template<T, C>::value;
+
 }  // namespace nu
 
 #include "nu/impl/type_traits.ipp"
