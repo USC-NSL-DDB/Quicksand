2e79240451634113e471be88d307e5b38d9e3e9f
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2022-08-25 07:14:55 -0600

    bench: set up ShardedUnorderedMap benchmark infra

---
+++ bench/bench_sharded_unordered_map.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cereal/types/string.hpp>
+#include <functional>
+#include <iostream>
+
+#include "nu/proclet.hpp"
+#include "nu/runtime.hpp"
+#include "nu/sharded_unordered_map.hpp"
+
+constexpr uint32_t kRunTimes = 1;
+
+template <typename F>
+inline uint64_t time(F fn) {
+  auto t0 = microtime();
+  fn();
+  auto t1 = microtime();
+  return t1 - t0;
+}
+
+void bench_insert_ints() {
+  int num_pairs = 2'000'000;
+  {
+    auto m = nu::make_sharded_unordered_map<int, int, std::false_type>();
+
+    auto insertion_time = time([&] {
+      for (int i = 0; i < num_pairs; i++) {
+        m.emplace(i, i);
+      }
+      m.flush();
+    });
+
+    std::cout << "\t\tShardedUnorderedMap:\t" << insertion_time << " us"
+              << std::endl;
+  }
+
+  {
+    nu::RuntimeSlabGuard slab;
+    std::unordered_map<int, int> m;
+
+    auto insertion_time = time([&] {
+      for (int i = 0; i < num_pairs; i++) {
+        m[i] = i;
+      }
+    });
+
+    std::cout << "\t\tstd::unordered_map:\t" << insertion_time << " us"
+              << std::endl;
+  }
+}
+
+int main(int argc, char **argv) {
+  return nu::runtime_main_init(argc, argv,
+                               [](int, char **) { bench_insert_ints(); });
+}
