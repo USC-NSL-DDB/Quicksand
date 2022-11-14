62ae3146ef006c64a8459207dd5dcc2b7072dc63
Zhenyuan Ruan <zainryan95@gmail.com>
2022-11-13 23:36:52 -0500

    test: update test_continuous_migrate.cpp.

--- test/test_continuous_migrate.cpp
+++ test/test_continuous_migrate.cpp
@@ -1,10 +1,10 @@
 #include <algorithm>
-#include <cstddef>
 #include <cstdint>
 #include <iostream>
 #include <random>
 #include <ranges>
 #include <tuple>
+#include <type_traits>
 
 #include "nu/pressure_handler.hpp"
 #include "nu/proclet.hpp"
@@ -13,69 +13,122 @@
 
 using namespace nu;
 
-constexpr static uint32_t kNumProclets = 8192;
-constexpr static uint32_t kConcurrency = 1;
+constexpr static uint32_t kNumRootObjs = 2048;
 constexpr static uint32_t kProcletHeapSize = 2 << 20;
-constexpr static uint32_t kMinComputeTimeUs = 10;
-constexpr static uint32_t kMaxComputeTimeUs = 1000;
 constexpr static uint32_t kArgsSize = 100;
+constexpr static uint32_t kNumTiers = 3;
+constexpr static uint32_t kFanOuts = 2;
+constexpr static uint32_t kMinComputeTimeUs = 10;
+constexpr static uint32_t kMaxComputeTimeUs = 200;
+constexpr static uint32_t kMigrationIntervalUs = 10 * kOneSecond;
+constexpr static uint32_t kNumMigrationSourceNodes = 2;
+constexpr static uint32_t kClientConcurrency = 20;
 
 struct Args {
   uint8_t data[kArgsSize];
 };
 
-class Obj {
-public:
- Obj() : rd_(), gen_(rd_()), dis_(kMinComputeTimeUs, kMaxComputeTimeUs) {}
+class IntGen {
+ public:
+  IntGen(int left, int right) : rd_(), gen_(rd_()), dis_(left, right) {}
+  int next() { return dis_(gen_); }
 
- Args compute(Args args) {
-   for (auto &byte : args.data) {
-     ++byte;
-   }
-   Time::delay(dis_(gen_));
-   return args;
- }
-
-private:
- std::random_device rd_;
- std::mt19937 gen_;
- std::uniform_real_distribution<> dis_;
- std::byte data_[kProcletHeapSize];
+ private:
+  std::random_device rd_;
+  std::mt19937 gen_;
+  std::uniform_int_distribution<> dis_;
 };
 
-void do_work() {
-  std::vector<Proclet<Obj>> proclets;
-  for (uint32_t i = 0; i < kNumProclets; i++) {
-    proclets.emplace_back(nu::make_proclet<Obj>());
+template <int Tier>
+class Obj {
+ public:
+  constexpr static auto kIsLeaf = (Tier == kNumTiers - 1);
+
+  Obj() {
+    std::ranges::fill(data_, 0);
+    if constexpr (!kIsLeaf) {
+      for (auto &sub_obj : sub_objs_) {
+        sub_obj = nu::make_proclet<Obj<Tier + 1>>();
+      }
+    }
   }
 
-  for (uint32_t i = 0; i < kConcurrency; i++) {
-    rt::Spawn([&] {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_real_distribution<> dis(0, proclets.size());
+  Args compute(Args args, int time_us) {
+    Time::delay(time_us);
 
+    for (auto &byte : args.data) {
+      byte = 1;
+    }
+
+    if constexpr (!kIsLeaf) {
+      std::vector<Future<Args>> futures;
+      for (auto &sub_obj : sub_objs_) {
+        futures.emplace_back(
+            sub_obj.run_async(&Obj<Tier + 1>::compute, args, time_us));
+      }
+      for (uint32_t i = 0; i < kArgsSize; i++) {
+        for (auto &future : futures) {
+          args.data[i] += future.get().data[i];
+        }
+      }
+    }
+
+    return args;
+  }
+
+ private:
+  uint8_t data_[kProcletHeapSize];
+  std::conditional_t<!kIsLeaf, Proclet<Obj<Tier + 1>>, nu::ErasedType>
+      sub_objs_[kFanOuts];
+};
+
+consteval uint32_t num_nodes_per_root() {
+  uint64_t p = 1;
+  for (uint32_t i = 0; i < kNumTiers; i++) {
+    p *= kFanOuts;
+  }
+  return (p - 1) / (kFanOuts - 1);
+}
+
+void do_work() {
+  std::vector<Proclet<Obj<0>>> roots;
+  for (uint32_t i = 0; i < kNumRootObjs; i++) {
+    roots.emplace_back(nu::make_proclet<Obj<0>>());
+  }
+
+  for (uint32_t i = 0; i < kClientConcurrency; i++) {
+    rt::Spawn([&] {
+      IntGen idx_gen(0, roots.size() - 1);
+      IntGen time_gen(kMinComputeTimeUs, kMaxComputeTimeUs);
       Args args;
       std::ranges::fill(args.data, 0);
 
       while (true) {
-        auto &proclet = proclets[dis(gen)];
-        auto new_args = proclet.run(&Obj::compute, args);
+        auto &root = roots[idx_gen.next()];
+        auto rets = root.run(&Obj<0>::compute, args, time_gen.next());
 
-        for (auto pair : std::views::zip(new_args.data, args.data)) {
-          BUG_ON(std::get<0>(pair) != std::get<1>(pair) + 1);
-        }
-	if (proclet == proclets[0]) {
-          proclet.run(+[](Obj &_) {
-            Caladan::PreemptGuard g;
-            get_runtime()->pressure_handler()->mock_set_pressure();
-          });
+        for (auto n : rets.data) {
+          BUG_ON(n !=num_nodes_per_root());
         }
       }
     });
   }
 
-  get_runtime()->caladan()->thread_park();
+  uint64_t last_time_us = microtime();
+  IntGen idx_gen(0, roots.size() - 1);
+  while (true) {
+    auto now_time_us = microtime();
+    if (now_time_us >= last_time_us + kMigrationIntervalUs) {
+      last_time_us = now_time_us;
+      for (uint32_t i = 0; i < kNumMigrationSourceNodes; i++) {
+        auto &root = roots[idx_gen.next()];
+        root.run(+[](Obj<0> &_) {
+          Caladan::PreemptGuard g;
+          get_runtime()->pressure_handler()->mock_set_pressure();
+        });
+      }
+    }
+  }
 }
 
 int main(int argc, char **argv) {
