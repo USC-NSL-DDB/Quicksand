bc63b0cadeadb6f841e489edbea97f2d1fc7666d
Zhenyuan Ruan <zainryan95@gmail.com>
2022-11-24 19:50:52 -0500

    caladan: reduce IAS_RP_INTERVAL_US to 100.

--- caladan/iokernel/ias.h
+++ caladan/iokernel/ias.h
@@ -22,7 +22,7 @@
 /* the interval to trigger PS subcontroller */
 #define IAS_PS_INTERVAL_US              100
 /* the interval to trigger RP subcontroller */
-#define IAS_RP_INTERVAL_US              500
+#define IAS_RP_INTERVAL_US              100
 /* the time before the core-local cache is assumed to be evicted */
 #define IAS_LOC_EVICTED_US		100
 /* the debug info printing interval */
