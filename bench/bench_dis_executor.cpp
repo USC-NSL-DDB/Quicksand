e912d95de6820a738661649a5cec0d9ecd8d0749
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2023-01-09 22:53:30 -0500

    bench: set up produce-side burst in queue rate matching

--- bench/bench_dis_executor.cpp
+++ bench/bench_dis_executor.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <cstring>
 #include <functional>
 #include <iostream>
@@ -6,19 +7,23 @@
 #include "nu/dis_executor.hpp"
 #include "nu/proclet.hpp"
 #include "nu/runtime.hpp"
+#include "nu/sharded_queue.hpp"
 #include "nu/utils/time.hpp"
 #include "nu/vector_task_range.hpp"
 
 constexpr uint32_t kDelayUs = nu::kOneMilliSecond;
 
-void compute() {
-  const auto num_iters = kDelayUs * cycles_per_us;
+template <uint32_t Micros>
+void compute_us() {
+  const auto num_iters = Micros * cycles_per_us;
   for (uint32_t i = 0; i < num_iters; i++) {
     asm volatile("nop");
   }
 }
 
-void do_work() {
+void bench_vector_task_range() {
+  std::cout << "bench_vector_task_range" << std::endl;
+
   constexpr std::size_t kSize = (1 << 20);
 
   std::vector<std::size_t> inputs;
@@ -35,7 +40,7 @@ void do_work() {
         std::vector<std::size_t> outputs;
         while (!task_range.empty()) {
           task_range.pop();
-          compute();
+          compute_us<kDelayUs>();
         }
         return outputs;
       },
@@ -47,7 +52,57 @@ void do_work() {
   auto t1 = microtime();
   barrier();
 
-  std::cout << t1 - t0 << std::endl;
+  std::cout << "\t" << t1 - t0 << std::endl;
+}
+
+void bench_rate_match() {
+  std::cout << "bench_rate_match" << std::endl;
+
+  constexpr uint32_t kNormalDelayUs = 1000;
+  constexpr uint32_t kBurstDelayUs = 100;
+
+  auto queue = nu::make_sharded_queue<int, std::true_type>();
+
+  auto produce_rng = nu::make_writeable_queue_range(queue);
+  auto consume_rng = nu::make_queue_range(queue);
+
+  auto t0 = microtime();
+  auto t_scale_up = t0 + nu::kOneSecond * 3;
+  auto t_scale_down = t0 + nu::kOneSecond * 6;
+
+  auto producers = nu::make_distributed_executor(
+      +[](decltype(produce_rng) &rng, uint64_t t_scale_up,
+          uint64_t t_scale_down) {
+        while (true) {
+          auto inserter = rng.pop();
+          inserter = 33;
+
+          auto curr = microtime();
+          if (curr >= t_scale_up && curr <= t_scale_down) {
+            compute_us<kBurstDelayUs>();
+          } else {
+            compute_us<kNormalDelayUs>();
+          }
+        }
+      },
+      produce_rng, t_scale_up, t_scale_down);
+
+  auto consumers = nu::make_distributed_executor(
+      +[](decltype(consume_rng) &rng) {
+        while (true) {
+          rng.pop();
+          compute_us<kNormalDelayUs>();
+        }
+      },
+      consume_rng);
+
+  consumers.get();
+  producers.get();
+}
+
+void do_work() {
+  bench_vector_task_range();
+  // bench_rate_match();
 }
 
 int main(int argc, char **argv) {
