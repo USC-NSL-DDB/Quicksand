6d53721f7724dddfbb0961fef77e0e37c9cf466d
Kaiyan Fan <nbfanks@gmail.com>
2023-04-09 00:26:40 -0600

    Excamera: wip compute proclets on entire pipeline

--- app/excamera/src/xc_qs.cpp
+++ app/excamera/src/xc_qs.cpp
@@ -11,6 +11,7 @@
 #include <filesystem>
 #include <vector>
 #include <functional>
+#include <tuple>
 
 #include <cereal/archives/binary.hpp>
 #include <cereal/types/vector.hpp>
@@ -23,6 +24,7 @@
 #include "nu/sharded_vector.hpp"
 #include "nu/dis_executor.hpp"
 #include "nu/sharded_ds_range.hpp"
+#include "nu/zipped_ds_range.hpp"
 
 #include "ivf.hh"
 #include "uncompressed_chunk.hh"
@@ -44,6 +46,7 @@ using namespace chrono;
 namespace fs = filesystem;
 
 size_t N = 16;
+string prefix;
 
 // serialized decoder state
 using DecoderBuffer = vector<uint8_t>;
@@ -52,20 +55,49 @@ using shard_ivf_type = nu::ShardedVector<IVF_MEM, false_type>;
 using shard_decoder_type = nu::ShardedVector<DecoderBuffer, false_type>;
 using sealed_shard_ivf = nu::SealedDS<shard_ivf_type>;
 using sealed_shard_decoder = nu::SealedDS<shard_decoder_type>;
+using shard_idx_type = nu::ShardedVector<size_t, false_type>;
+using sealed_shard_idx = nu::SealedDS<shard_idx_type>;
 
 // The excamera states
 typedef struct {
-  vector<IVF_MEM> xc0_ivf, xc1_ivf, rebased_ivf, final_ivf;
-  vector<DecoderBuffer> dec_state, enc0_state, enc1_state, rebased_state;
-  vector<vector<RasterHandle>> rasters;
+  shard_ivf_type vpx0_ivf, vpx1_ivf, xc0_ivf, xc1_ivf;
+  shard_decoder_type dec_state, enc0_state, enc1_state;
+  vector<IVF_MEM> final_ivf;
+  vector<DecoderBuffer> rebased_state;
+  IVF_MEM ivf0;
+  DecoderBuffer state0;
   uint16_t display_width, display_height;
-  shard_ivf_type vpx_ivf;
 } xc_t;
 
 vector<uint32_t> stage1_time, stage2_time, stage3_time;
 
 void usage(const string &program_name) {
   cerr << "Usage: " << program_name << " [nu_args] [input_dir]" << endl;
+}
+
+tuple<vector<RasterHandle>, uint16_t, uint16_t> read_raster(size_t idx) {
+  ostringstream inputss;
+  inputss << prefix << setw(2) << setfill('0') << idx << ".y4m";
+  const string input_file = inputss.str();
+  cout << input_file << endl;
+
+  shared_ptr<FrameInput> input_reader = make_shared<YUV4MPEGReader>( input_file );
+  IVFWriter_MEM ivf_writer { "VP80", input_reader->display_width(), input_reader->display_height(), 1, 1 };
+
+  // pre-read original rasters
+  vector<RasterHandle> original_rasters;
+  for ( size_t i = 0; ; i++ ) {
+    auto next_raster = input_reader->get_next_frame();
+
+    if ( next_raster.initialized() ) {
+      original_rasters.emplace_back( next_raster.get() );
+    } else {
+      break;
+    }
+  }
+
+  tuple<vector<RasterHandle>, uint16_t, uint16_t> output(original_rasters, input_reader->display_width(), input_reader->display_height());
+  return output;
 }
 
 DecoderBuffer decode(IVF_MEM &ivf) {
@@ -97,31 +129,30 @@ DecoderBuffer decode(IVF_MEM &ivf) {
   throw runtime_error( "invalid frame number" );
 }
 
-void enc_given_state(vector<IVF_MEM> & output_ivf,
-                     vector<DecoderBuffer> & input_state, 
-                     vector<DecoderBuffer> & output_state,
-                     vector<IVF_MEM> & pred,
-                     vector<DecoderBuffer> * prev_state,
-                     vector<RasterHandle> & rasters,
-                     uint16_t display_width, uint16_t display_height,
-                     size_t index) {
+tuple<IVF_MEM, DecoderBuffer>
+enc_given_state(IVF_MEM & pred_ivf,
+                DecoderBuffer & input_state,
+                DecoderBuffer * prev_state,
+                size_t idx) {
   bool two_pass = false;
   double kf_q_weight = 1.0;
   bool extra_frame_chunk = false;
   EncoderQuality quality = BEST_QUALITY;
 
+  auto rasters = read_raster(idx);
+  vector<RasterHandle> original_rasters = get<0>(rasters);
+  uint16_t display_width = get<1>(rasters);
+  uint16_t display_height = get<2>(rasters);
+
   Decoder pred_decoder( display_width, display_height );
   if (prev_state) {
-    pred_decoder = EncoderStateDeserializer_MEM::build<Decoder>( prev_state->at(index - 1) );
+    pred_decoder = EncoderStateDeserializer_MEM::build<Decoder>( *prev_state );
   }
 
   IVFWriter_MEM ivf_writer { "VP80", display_width, display_height, 1, 1 };
-  vector<RasterHandle> original_rasters = rasters;
 
   /* pre-read all the prediction frames */
   vector<pair<Optional<KeyFrame>, Optional<InterFrame> > > prediction_frames;
-
-  IVF_MEM pred_ivf = pred[index];
 
   if ( not pred_decoder.minihash_match( pred_ivf.expected_decoder_minihash() ) ) {
     throw Invalid( "Mismatch between prediction IVF and prediction_ivf_initial_state" );
@@ -143,23 +174,20 @@ void enc_given_state(vector<IVF_MEM> & output_ivf,
     }
   }
 
-  Encoder encoder( EncoderStateDeserializer_MEM::build<Decoder>(input_state[index-1]) , two_pass, quality );
+  Encoder encoder( EncoderStateDeserializer_MEM::build<Decoder>(input_state) , two_pass, quality );
 
   ivf_writer.set_expected_decoder_entry_hash( encoder.export_decoder().get_hash().hash() );
 
   encoder.reencode( original_rasters, prediction_frames, kf_q_weight,
                     extra_frame_chunk, ivf_writer );
 
-  output_ivf[index] = ivf_writer.ivf();
   EncoderStateSerializer odata = {};
   encoder.export_decoder().serialize(odata);
-  output_state[index] = odata.get();
+  tuple<IVF_MEM, DecoderBuffer> output(ivf_writer.ivf(), odata.get());
+  return output;
 }
 
-void merge(vector<IVF_MEM> & input1, vector<IVF_MEM> & input2, vector<IVF_MEM> & output, size_t index) {
-  IVF_MEM ivf1 = input1[index-1];
-  IVF_MEM ivf2 = input2[index];
-
+IVF_MEM merge(IVF_MEM & ivf1, IVF_MEM & ivf2) {
   if ( ivf1.width() != ivf2.width() or ivf1.height() != ivf2.height() ) {
     throw runtime_error( "cannot merge ivfs with different dimensions." );
   }
@@ -174,11 +202,11 @@ void merge(vector<IVF_MEM> & input1, vector<IVF_MEM> & input2, vector<IVF_MEM> &
     ivf_writer.append_frame( ivf2.frame( i ) );
   }
 
-  output[index] = ivf_writer.ivf();
+  return ivf_writer.ivf();
 }
 
-void decode_all(const string prefix, xc_t &s) {
-  auto sealed_ivfs = nu::to_sealed_ds(move(s.vpx_ivf));
+void decode_all(xc_t &s) {
+  auto sealed_ivfs = nu::to_sealed_ds(move(s.vpx0_ivf));
   auto ivfs_range = nu::make_contiguous_ds_range(sealed_ivfs);
   
   auto dis_exec = nu::make_distributed_executor(
@@ -197,149 +225,193 @@ void decode_all(const string prefix, xc_t &s) {
 
   auto outputs_vectors = dis_exec.get();
   auto join_view = ranges::join_view(outputs_vectors);
-  s.dec_state = vector<DecoderBuffer>(join_view.begin(), join_view.end());
-  s.vpx_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
+  auto vecs = vector<DecoderBuffer>(join_view.begin(), join_view.end());
+  s.state0 = vecs[0];
+  for (size_t i = 0; i < N-1; i++) {
+    s.dec_state.push_back(vecs[i]);
+  }
+  s.vpx0_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
 }
 
-void encode_all(const string prefix, xc_t &s) {
-  vector<IVF_MEM> ivfs;
-  auto sealed_ivfs = nu::to_sealed_ds(std::move(s.vpx_ivf));
-  for (auto it = sealed_ivfs.cbegin(); it != sealed_ivfs.cend(); ++it) {
-    ivfs.push_back(*it);
+void encode_all(xc_t &s) {
+  shard_idx_type idxs = nu::make_sharded_vector<size_t, false_type>(N-1);
+  for (size_t i = 1; i < N; i++) {
+    idxs.push_back(i);
   }
-  s.vpx_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
 
-  vector <rt::Thread> ths;
   // first pass
-  for (size_t i = 0; i < N; i++) {    
-    if (i == 0) {
-      s.xc0_ivf[0] = ivfs[0];
-      s.enc0_state[0] = s.dec_state[0];
-    } else {
-      ths.emplace_back( [i, &s, &ivfs] {
-        auto start = microtime();
-        enc_given_state(s.xc0_ivf, s.dec_state, s.enc0_state, ivfs, nullptr, s.rasters[i], s.display_width, s.display_height, i);
-        auto end = microtime();
-        stage2_time[i] = end - start;
-      } );
-    }
-  }
-  for (auto &th : ths) {
-    th.Join();
-  }
+  {
+    auto sealed_ivfs = nu::to_sealed_ds(move(s.vpx1_ivf));
+    auto sealed_dec_state = nu::to_sealed_ds(move(s.dec_state));
+    auto sealed_idxs = nu::to_sealed_ds(move(idxs));
+    auto encode_range = nu::make_zipped_ds_range(sealed_ivfs, sealed_dec_state, sealed_idxs);
 
-  ths.clear();
+    auto dist_exec = nu::make_distributed_executor(
+      +[](decltype(encode_range) &encode_range) {
+        vector<tuple<IVF_MEM, DecoderBuffer>> outputs;
+        while (true) {
+          auto t = encode_range.pop();
+          if (!t) {
+            break;
+          }
+          auto ivf = get<0>(*t);
+          auto decoder = get<1>(*t);
+          auto idx = get<2>(*t);
+          outputs.push_back(enc_given_state(ivf, decoder, nullptr, idx));
+        }
+        return outputs;
+      }, encode_range);
+
+    auto outputs_vectors = dist_exec.get();
+    auto join_view = ranges::join_view(outputs_vectors);
+    auto vecs = vector<tuple<IVF_MEM, DecoderBuffer>>(join_view.begin(), join_view.end());
+
+    s.vpx1_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
+    s.dec_state = nu::to_unsealed_ds(move(sealed_dec_state));
+    idxs = nu::to_unsealed_ds(move(sealed_idxs));
+
+    s.enc0_state.push_back(s.state0);
+    for (size_t i = 0; i < N-2; i++) {
+      s.xc0_ivf.push_back(get<0>(vecs[i]));
+      s.enc0_state.push_back(get<1>(vecs[i]));
+    }
+    s.xc0_ivf.push_back(get<0>(vecs[N-1]));
+  }
 
   // second pass
-  for (size_t i = 0; i < N; i++) {    
-    if (i == 0) {
-      s.xc1_ivf[0] = s.xc0_ivf[0];
-      s.enc1_state[0] = s.enc0_state[0];
-    } else {
-      vector<DecoderBuffer> *prev_state_ptr = &(s.dec_state);
-      ths.emplace_back( [i, prev_state_ptr, &s] {
-        auto start = microtime();
-        enc_given_state(s.xc1_ivf, s.enc0_state, s.enc1_state, s.xc0_ivf, prev_state_ptr, s.rasters[i], s.display_width, s.display_height, i);
-        auto end = microtime();
-        stage2_time[i] += end - start;
-      } );
+  {
+    auto sealed_ivfs = nu::to_sealed_ds(move(s.xc0_ivf));
+    auto sealed_enc0_state = nu::to_sealed_ds(move(s.enc0_state));
+    auto sealed_dec_state = nu::to_sealed_ds(move(s.dec_state));
+    auto sealed_idxs = nu::to_sealed_ds(move(idxs));
+    auto encode_range = nu::make_zipped_ds_range(sealed_ivfs, sealed_enc0_state, sealed_dec_state, sealed_idxs);
+
+    auto dist_exec = nu::make_distributed_executor(
+      +[](decltype(encode_range) &encode_range) {
+        vector<tuple<IVF_MEM, DecoderBuffer>> outputs;
+        while (true) {
+          auto t = encode_range.pop();
+          if (!t) {
+            break;
+          }
+          auto ivf = get<0>(*t);
+          auto decoder = get<1>(*t);
+          auto prev_decoder = get<2>(*t);
+          auto idx = get<3>(*t);
+          outputs.push_back(
+            enc_given_state(ivf, decoder, &prev_decoder, idx)
+          );
+        }
+        return outputs;
+      }, encode_range);
+
+    auto outputs_vectors = dist_exec.get();
+    auto join_view = ranges::join_view(outputs_vectors);
+    auto vecs = vector<tuple<IVF_MEM, DecoderBuffer>>(join_view.begin(), join_view.end());
+
+    s.xc0_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
+    s.enc0_state = nu::to_unsealed_ds(move(sealed_enc0_state));
+    s.dec_state = nu::to_unsealed_ds(move(sealed_dec_state));
+    idxs = nu::to_unsealed_ds(move(sealed_idxs));
+
+    s.enc1_state.push_back(s.state0);
+    for (size_t i = 0; i < N-2; i++) {
+      s.xc1_ivf.push_back(get<0>(vecs[i]));
+      s.enc1_state.push_back(get<1>(vecs[i]));
     }
-  }
-  for (auto &th : ths) {
-    th.Join();
+    s.xc1_ivf.push_back(get<0>(vecs[N-1]));
   }
 }
 
-void rebase(const string prefix, xc_t &s) {
+void rebase(xc_t &s) {
+  s.final_ivf[0] = s.ivf0;
+  s.rebased_state[0] = s.state0;
+
+  vector<IVF_MEM> xc1_ivf;
+  auto sealed_ivfs = nu::to_sealed_ds(std::move(s.xc1_ivf));
+  for (auto it = sealed_ivfs.cbegin(); it != sealed_ivfs.cend(); ++it) {
+    xc1_ivf.push_back(*it);
+  }
+  s.xc1_ivf = nu::to_unsealed_ds(move(sealed_ivfs));
+
+  vector<DecoderBuffer> enc0_state;
+  auto sealed_enc0_state = nu::to_sealed_ds(std::move(s.enc0_state));
+  for (auto it = sealed_enc0_state.cbegin(); it != sealed_enc0_state.cend(); ++it) {
+    enc0_state.push_back(*it);
+  }
+  s.enc0_state = nu::to_unsealed_ds(move(sealed_enc0_state));
+
   // serial rebase
-  for (size_t i = 0; i < N; i++) {
-    if (i == 0) {
-      s.final_ivf[0] = s.xc1_ivf[0];
-      s.rebased_ivf[0] = s.xc1_ivf[0];
-      s.rebased_state[0] = s.enc0_state[0];
-    } else {
-      auto start = microtime();
-      enc_given_state(s.rebased_ivf, s.rebased_state, s.rebased_state, s.xc1_ivf, &(s.enc0_state), s.rasters[i], s.display_width, s.display_height, i);
-      merge(s.final_ivf, s.rebased_ivf, s.final_ivf, i);
-      auto end = microtime();
-      stage3_time[i] = end - start;
-    }
+  for (size_t i = 1; i < N; i++) {
+    // auto start = microtime();
+    IVF_MEM ivf = xc1_ivf[i-1];
+    DecoderBuffer decoder = s.rebased_state[i-1];
+    DecoderBuffer prev_decoder = enc0_state[i-1];
+    auto output = enc_given_state(ivf, decoder, &prev_decoder, i);
+
+    auto rebased_ivf = get<0>(output);
+    s.rebased_state.push_back(get<1>(output));
+
+    s.final_ivf[i] = merge(s.final_ivf[i-1], rebased_ivf);
+    // auto end = microtime();
+    // stage3_time[i] = end - start;
   }
 }
 
-void write_output(const string prefix, xc_t &s) {
+void write_output(xc_t &s) {
   const string final_file = prefix + "final.ivf";
   s.final_ivf[N-1].write(final_file);
 }
 
-void read_input(const string prefix, xc_t &s) {
+void read_input(xc_t &s) {
   for (size_t i = 0; i < N; i++) {
-    ostringstream inputss, vpxss;
-    inputss << prefix << setw(2) << setfill('0') << i << ".y4m";
-    const string input_file = inputss.str();
-    
-    shared_ptr<FrameInput> input_reader = make_shared<YUV4MPEGReader>( input_file );
-    if (i == 0) {
-      s.display_width = input_reader->display_width();
-      s.display_height = input_reader->display_height();
-    }
-
-    IVFWriter_MEM ivf_writer { "VP80", input_reader->display_width(), input_reader->display_height(), 1, 1 };
-    // pre-read original rasters
-    vector<RasterHandle> original_rasters;
-    for ( size_t i = 0; ; i++ ) {
-      auto next_raster = input_reader->get_next_frame();
-
-      if ( next_raster.initialized() ) {
-        original_rasters.emplace_back( next_raster.get() );
-      } else {
-        break;
-      }
-    }
-    s.rasters.push_back(original_rasters);
-
+    ostringstream vpxss;
     vpxss << prefix << "vpx_" << setw(2) << setfill('0') << i << ".ivf";
     const string vpx_file = vpxss.str();
-    s.vpx_ivf.push_back(move(IVF_MEM(vpx_file)));
+    if (i != N - 1) {
+      s.vpx0_ivf.push_back(IVF_MEM(vpx_file));
+    }
+    if (i != 0) {
+      s.vpx1_ivf.push_back(IVF_MEM(vpx_file));
+    } else {
+      s.ivf0 = IVF_MEM(vpx_file);
+    }
   }
 }
 
-int do_work(const string & prefix) {
-  // if (argc != 2) {
-  //   usage(argv[0]);
-  //   return EXIT_FAILURE;
-  // }
-
+int do_work() {
   xc_t s;
 
-  s.vpx_ivf = nu::make_sharded_vector<IVF_MEM, std::false_type>(N);
-  s.xc0_ivf.resize(N);
-  s.xc1_ivf.resize(N);
-  s.rebased_ivf.resize(N);
+  s.vpx0_ivf = nu::make_sharded_vector<IVF_MEM, false_type>(N-1);
+  s.vpx1_ivf = nu::make_sharded_vector<IVF_MEM, false_type>(N-1);
+  s.xc0_ivf = nu::make_sharded_vector<IVF_MEM, false_type>(N-1);
+  s.xc1_ivf = nu::make_sharded_vector<IVF_MEM, false_type>(N-1);
+  s.dec_state = nu::make_sharded_vector<DecoderBuffer, false_type>(N-1);
+  s.enc0_state = nu::make_sharded_vector<DecoderBuffer, false_type>(N-1);
+  s.enc1_state = nu::make_sharded_vector<DecoderBuffer, false_type>(N-1);
+
   s.final_ivf.resize(N);
-  s.enc0_state.resize(N);
-  s.enc1_state.resize(N);
   s.rebased_state.resize(N);
 
   stage1_time.resize(N);
   stage2_time.resize(N);
   stage3_time.resize(N);
 
-  read_input(prefix, s);
+  read_input(s);
   auto t1 = microtime();
-  decode_all(prefix, s);
+  decode_all(s);
   auto t2 = microtime();
-  encode_all(prefix, s);
+  encode_all(s);
   auto t3 = microtime();
-  rebase(prefix, s);
+  rebase(s);
   auto t4 = microtime();
-  write_output(prefix, s);
+  write_output(s);
 
   auto stage1 = t2 - t1;
   auto stage2 = t3 - t2;
   auto stage3 = t4 - t3;
 
-  cout << "decode: " << stage1 << ". encode_given_state: " << stage2 << ". rebase: " << stage3 << "." << endl;
+  cout << "decode: " << stage1 << ". enc_given_state: " << stage2 << ". rebase: " << stage3 << "." << endl;
 
   cout << "Stage 1: {";
   for (auto t : stage1_time) {
@@ -362,6 +434,6 @@ int do_work(const string & prefix) {
 
 int main(int argc, char *argv[]) {
   // TODO: take file prefix to args
-  string prefix = string(argv[argc-1]) + "/sintel01_";
-  return nu::runtime_main_init(argc, argv, [=](int, char **) { do_work(prefix); });
+  prefix = string(argv[argc-1]) + "/sintel01_";
+  return nu::runtime_main_init(argc, argv, [=](int, char **) { do_work(); });
 }
