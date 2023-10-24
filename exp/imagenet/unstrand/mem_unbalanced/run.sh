173f4d6434420e76283247e975b4b050a79f0e0d
Zain Ruan <zainryan95@gmail.com>
2023-10-24 18:13:51 +0000

    exp: add the script for the imagenet mem-unbalanced exp.

--- exp/imagenet/unstrand/mem_unbalanced/run.sh
+++ exp/imagenet/unstrand/mem_unbalanced/run.sh
@@ -8,11 +8,13 @@ SRV_STARTING_IDX=1
 NUM_SRVS=6
 MAIN_SRV_IDX=$(expr $SRV_STARTING_IDX + $NUM_SRVS - 1)
 GPU_IDX=$(expr $MAIN_SRV_IDX + 1)
-KS_FIRST_HALF=6
-KS_LATTER_HALF=20
+KS_FIRST_HALF=13
+KS_LATTER_HALF=13
 GPU_KS=0
 GPU_SPIN_KS=0
 DIR=`pwd`
+SRV1_APP_MEM=1024
+NU_LOW_MEM=1024
 
 function get_srv_idx() {
     echo $(expr $SRV_STARTING_IDX + $1 - 1)
@@ -66,7 +68,22 @@ do
     then
 	LD_LIBRARY_PATH=`pwd`/opencv/install/lib \
 		       start_server distributed $(get_srv_idx $i) $LPID $ks >$DIR/logs/srv.$i 2>&1 &
-    fi    
+    fi
+done
+
+sleep 5
+
+mem_antagonist=$NU_DIR/bin/bench_real_mem_pressure
+mem_target=$(expr $SRV1_APP_MEM + $NU_LOW_MEM)
+for i in `seq 1 $(expr $NUM_SRVS / 2)`
+do
+    srv_idx=$(get_srv_idx $i)
+    run_program $mem_antagonist $srv_idx $DIR/antagonist$i.conf $mem_target 0 >$DIR/logs/antagonist.$i &
+done
+
+for i in `seq 1 $(expr $NUM_SRVS / 2)`
+do
+    ( tail -f $DIR/logs/antagonist.$i & ) | grep -q "waiting for signal"
 done
 
 sleep 5
