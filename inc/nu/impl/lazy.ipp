7d8569b72aaa7f2bf32282320f715e806e05163f
Zain Ruan <zainryan95@gmail.com>
2023-11-23 03:04:46 +0000

    utils: extend Lazy to support transform and move-only captures.

--- inc/nu/impl/lazy.ipp
+++ inc/nu/impl/lazy.ipp
@@ -14,11 +14,29 @@ bool Lazy<T>::is_evaluated() const {
 }
 
 template <typename T>
-Lazy<T>::Lazy(std::function<T()> &&fn) : fn_(std::move(fn)) {}
+Lazy<T>::Lazy(std::move_only_function<T()> &&fn) : fn_(std::move(fn)) {}
 
 template <typename T>
-Lazy<T> make_lazy(std::function<T()> fn) {
-  return Lazy<T>(std::move(fn));
+Lazy<T>::Lazy(T &&t) : t_(std::move(t)) {}
+
+template <typename T, typename F>
+Lazy<T> make_lazy(F &&f)
+  requires(!std::is_same_v<T, F>)
+{
+  return Lazy<T>(std::forward<F>(f));
+}
+
+template <typename T>
+Lazy<T> make_lazy(T t) {
+  return Lazy<T>(std::move(t));
+}
+
+template <typename T, typename F>
+auto transform_lazy(Lazy<T> &&u, F &&f) -> Lazy<std::result_of_t<F(T)>> {
+  using RetT = std::result_of_t<F(T)>;
+  return Lazy<RetT>([u = std::move(u), f = std::forward<F>(f)]() mutable {
+    return f(std::move(u.get()));
+  });
 }
 
 }  // namespace nu
--- inc/nu/utils/lazy.hpp
+++ inc/nu/utils/lazy.hpp
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <optional>
+#include <type_traits>
 
 namespace nu {
 
@@ -12,16 +13,29 @@ class Lazy {
   bool is_evaluated() const;
 
  private:
-  Lazy(std::function<T()> &&fn);
+  Lazy(std::move_only_function<T()> &&fn);
+  Lazy(T &&t);
   template <typename U>
-  friend Lazy<U> make_lazy(std::function<U()>);
+  friend Lazy<U> make_lazy(U);
+  template <typename U, typename F>
+  friend Lazy<U> make_lazy(F &&)
+    requires(!std::is_same_v<U, F>);
+  template <typename U, typename F>
+  friend auto transform_lazy(Lazy<U> &&, F &&) -> Lazy<std::result_of_t<F(U)>>;
 
-  std::function<T()> fn_;
+  std::move_only_function<T()> fn_;
   std::optional<T> t_;
 };
 
 template <typename T>
-Lazy<T> make_lazy(std::function<T()> fn);
+Lazy<T> make_lazy(T t);
+
+template <typename T, typename F>
+Lazy<T> make_lazy(F &&f)
+  requires(!std::is_same_v<T, F>);
+
+template <typename T, typename F>
+auto transform_lazy(Lazy<T> &&u, F &&f) -> Lazy<std::result_of_t<F(T)>>;
 
 }  // namespace nu
 
