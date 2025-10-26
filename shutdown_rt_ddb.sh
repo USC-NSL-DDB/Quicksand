070fabe96fbd8278b7e8c3e65fce5095b57f6386
Yibo Yan <yiboyan@usc.edu>
2025-10-26 09:56:03 +0000

    mod: update runtime script

--- shutdown_rt_ddb.sh
+++ shutdown_rt_ddb.sh
@@ -2,8 +2,8 @@
 
 # Give a chance for graceful shutdown
 sudo pkill -INT iokerneld
-sudo pkill -INT ctrl_main
-sudo pkill -INT ctrl_proxy
+sudo pkill -INT ctrl_main  2>&1 > /dev/null
+sudo pkill -INT ctrl_proxy 2>&1 > /dev/null
 
 sleep 2
 sudo pkill -9 iokerneld 2>&1 > /dev/null
